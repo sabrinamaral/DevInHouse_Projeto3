0a2eba9cab215617a53b79d238fd7038c8beffc1
sabrinamaral <sabrinamaral@hotmail.com>
2022-01-20 15:31:55 -0300

    finish controller importFromXLS

--- src/controllers/financeController.js
+++ src/controllers/financeController.js
@@ -1,4 +1,8 @@
 const xlsxPopulate = require("xlsx-populate");
+const { getJsDateFromExcel } = require("excel-date-to-js");
+const { getJsonData, createUpdateUser } = require("../utils/functions");
+
+const transactions = getJsonData("finance.json");
 
 module.exports = {
   async importFromXLS(req, res) {
@@ -18,8 +22,24 @@ module.exports = {
         message: `Please, make your woksheet with these specific columns: price, typeOfExpenses, date, name, in this exactly order.`,
       });
     }
+    rows.shift();
+    rows.map((row) => {
+      if ([row[2]]) {
+        row[2] = getJsDateFromExcel(row[2]);
+      }
+      const result = row.map((cell, index) => {
+        return {
+          [firstRow[index]]: cell ? cell : "",
+        };
+      });
+      const idEntry = new Date().getTime();
+      const financeObj = Object.assign({}, { idEntry }, ...result);
+      transactions.push(financeObj);
+      createUpdateUser("finance.json", transactions);
+    });
+
     return res
       .status(200)
-      .send({ message: "POST finance endpoint working..." });
+      .send({ success: true, message: `Success! Entry registered.` });
   },
 };
