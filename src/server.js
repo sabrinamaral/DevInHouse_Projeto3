e1f795a0cfe8bb768a1c13b7b4428d7a10087d3a
sabrinamaral <sabrinamaral@hotmail.com>
2022-01-25 10:13:55 -0300

    add swagger

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const routes = require("./routes");
 
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("./swagger_output.json");
+
 const app = express();
+
+app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(express.json());
 app.use(routes);
