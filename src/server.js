e7963aacd006a1d9853df17bc753f5fe046211d9
sabrinamaral <sabrinamaral@hotmail.com>
2022-01-30 14:43:34 -0300

    change the PORT

--- src/server.js
+++ src/server.js
@@ -11,4 +11,4 @@ app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(express.json());
 app.use(routes);
 
-app.listen(3000, () => console.log("server listening on port 3000"));
+app.listen(3333, () => console.log("server listening on port 3333"));
